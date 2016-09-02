44c702b835b90301a79bd0d0622fccba8ebca74e
Phoebe Pan <phoebe_pan@hiiir.com>
2016-09-02 21:26:19 +0800

    [feature #1381] change: cahnge review change and continue

--- inject.js
+++ inject.js
@@ -5,10 +5,31 @@ function changeReviewButton() {
     console.log("!!! buttonReviewChanges", buttonReviewChanges);
 
     buttonReviewChanges.innerText = "AA Miner Review Changes";
-}
+
+    // change Continue button
+    buttonReviewChanges.onclick = function() {
+        setTimeout(function() {
+            changeButtonContinue();
+            console.log("!!! 000 10sec");
+        }, 3000);
+    };
+};
+
+function changeButtonContinue() {
+    var buttonContinue = document.getElementsByClassName("selected")[1];
+    var createDom = document.createElement('button');
+    var parentDom = buttonContinue.parentNode;
+    createDom.id = "updateformPE";
+    createDom.innerText = "AA Miner Continue"
+    createDom.style.display = "inline-block";
+    buttonContinue.style.display = "none";
+    parentDom.appendChild(createDom);
+
+    console.log("!!! 001 create new updateFromPE Dom!");
+};
 
 // Do something after FB loading end
 setTimeout(function() {
   console.log("!!! setTimeout 10000");
   changeReviewButton();
-}, 10000);
+}, 15000);
