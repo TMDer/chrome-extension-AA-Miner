df0f206debcc727cfa9b16a553dafb247be4f0d2
arvin_chen <arvin_chen@Arvin.local>
2016-09-05 18:00:17 +0800

    [feature #1456] 抓取「token」、「businessId」、「adAccountId」三個參數

--- background.js
+++ background.js
@@ -19,19 +19,19 @@ function checkUrl(tabId, changeInfo, tab) {
 
 // 只要 tab 更新就會在背景觸發比對網址是不是 "business.facebook.com"，決定要不要啟動 AAminer
 chrome.tabs.onUpdated.addListener(checkUrl);
-console.log("!!! background")
+console.log("!!! background");
 
+chrome.webRequest.onBeforeSendHeaders.addListener(function(details) {
 
-chrome.webRequest.onBeforeRequest.addListener(function(details) {
+	if(details.url.match("current_addrafts")){
+		var params =  details.url.split("/");
+		var token = params[5].split("&")[0].replace("current_addrafts?access_token=","");
+		var businessId = params[5].split("&")[2].replace("__business_id=","");
+		var adAccountId = params[4].replace("act_","");
 
-	if(details.url) {
-		stringifyDetails = JSON.stringify(details.url);
-		console.log("!!! 000webRequestBlocking details" + stringifyDetails);
+		console.log("token  : " , token);
+		console.log("businessId  : " , businessId);
+		console.log("adAccountId  : " , adAccountId);
 	}
 
-	if(details.requestBody) {
-		requestBodyFormDataQ= JSON.stringify(details.requestBody.formData.q);
-		console.log("!!! 111requestBodyFormDataQ" + requestBodyFormDataQ);
-	}
-
-}, {urls: ["https://business.facebook.com/ajax/bz"]}, ["requestBody"]);
+}, {urls: ["<all_urls>"]}, ["blocking", "requestHeaders"]);
