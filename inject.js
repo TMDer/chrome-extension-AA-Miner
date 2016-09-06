c9ebc82cc21404d7d6ac11edb7e8e0a4339830bf
Phoebe Pan <papashong1111@yahoo.com.tw>
2016-09-06 18:19:21 +0800

    [hotfix] 調整縮排：tab to space 2

--- inject.js
+++ inject.js
@@ -1,48 +1,48 @@
 function changeReviewButton() {
 
-    var buttonReviewChanges = document.getElementsByClassName("selected")[0];
+  var buttonReviewChanges = document.getElementsByClassName("selected")[0];
 
-    console.log("!!! buttonReviewChanges", buttonReviewChanges);
+  console.log("!!! buttonReviewChanges", buttonReviewChanges);
 
-    buttonReviewChanges.innerText = "AA Miner Review Changes";
+  buttonReviewChanges.innerText = "AA Miner Review Changes";
 
-    // change Continue button after review changes
-    buttonReviewChanges.onclick = function() {
-        setTimeout(function() {
-            changeButtonContinue();
-            console.log("!!! 000 3sec changeButtonContinue");
-        }, 3000);
-    };
+  // change Continue button after review changes
+  buttonReviewChanges.onclick = function() {
+    setTimeout(function() {
+        changeButtonContinue();
+        console.log("!!! 000 3sec changeButtonContinue");
+    }, 3000);
+  };
 };
 
 function changeButtonContinue() {
 
-    console.log("!!! 001 create new updateFromPE Dom!");
+  console.log("!!! 001 create new updateFromPE Dom!");
 
-    var originButtonContinue = document.getElementsByClassName("selected")[1];
-    var createDom = document.createElement('button');
-    var parentDom = originButtonContinue.parentNode;
-    createDom.id = "updateformPE";
-    createDom.innerText = "AA Miner Continue"
-    createDom.style.display = "inline-block";
-    originButtonContinue.style.display = "none";
-    parentDom.appendChild(createDom);
+  var originButtonContinue = document.getElementsByClassName("selected")[1];
+  var createDom = document.createElement('button');
+  var parentDom = originButtonContinue.parentNode;
+  createDom.id = "updateformPE";
+  createDom.innerText = "AA Miner Continue"
+  createDom.style.display = "inline-block";
+  originButtonContinue.style.display = "none";
+  parentDom.appendChild(createDom);
 
-    // send AA Miner API
-    // sendUpdateFromAAMiner();
+  // send AA Miner API
+  // sendUpdateFromAAMiner();
 
-    // need to add background.js
-    // chrome.runtime.sendMessage('Hello Taiwan');
+  // need to add background.js
+  // chrome.runtime.sendMessage('Hello Taiwan');
 
 };
 
 // AA Miner API
 function sendUpdateFromAAMiner() {
-    chrome.runtime.onMessage.addListener(function(response, sender, sendResponse){
-        alert(response);
-    });
+  chrome.runtime.onMessage.addListener(function(response, sender, sendResponse){
+    alert(response);
+  });
 
-    // send API
+  // send API
 };
 
 // Do something after FB loading end
