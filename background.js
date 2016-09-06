70fc87a4277da4ed742a6765ae7bfdcf05ee4cd2
Phoebe Pan <papashong1111@yahoo.com.tw>
2016-09-06 16:09:17 +0800

    [feature #1456] add "urlParams" for AA Miner API & delete "declarativeWebRequest" of manifest.json

--- background.js
+++ background.js
@@ -1,3 +1,5 @@
+var urlParams = {};
+
 function getDomain(url){
 	var host = "null";
 	if(typeof url == "undefined" || null == url)
@@ -19,7 +21,6 @@ function checkUrl(tabId, changeInfo, tab) {
 
 // 只要 tab 更新就會在背景觸發比對網址是不是 "business.facebook.com"，決定要不要啟動 AAminer
 chrome.tabs.onUpdated.addListener(checkUrl);
-console.log("!!! background");
 
 chrome.webRequest.onBeforeSendHeaders.addListener(function(details) {
 
@@ -29,9 +30,12 @@ chrome.webRequest.onBeforeSendHeaders.addListener(function(details) {
 		var businessId = params[5].split("&")[2].replace("__business_id=","");
 		var adAccountId = params[4].replace("act_","");
 
-		console.log("token  : " , token);
-		console.log("businessId  : " , businessId);
-		console.log("adAccountId  : " , adAccountId);
+		urlParams.token = token;
+		urlParams.businessId = businessId;
+		urlParams.adAccountId = adAccountId;
+
+		// todo: send message to content js for AA Miner API
+		console.log(urlParams);
 	}
 
 }, {urls: ["<all_urls>"]}, ["blocking", "requestHeaders"]);
