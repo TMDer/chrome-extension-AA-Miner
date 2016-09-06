c7edde13134d6302d3236e950c99f818069f15d7
Phoebe Pan <papashong1111@yahoo.com.tw>
2016-09-06 10:35:51 +0800

    [feature #1381] change continue button after 3 sec

--- inject.js
+++ inject.js
@@ -5,10 +5,48 @@ function changeReviewButton() {
     console.log("!!! buttonReviewChanges", buttonReviewChanges);
 
     buttonReviewChanges.innerText = "AA Miner Review Changes";
-}
+
+    // change Continue button after review changes
+    buttonReviewChanges.onclick = function() {
+        setTimeout(function() {
+            changeButtonContinue();
+            console.log("!!! 000 3sec changeButtonContinue");
+        }, 3000);
+    };
+};
+
+function changeButtonContinue() {
+
+    console.log("!!! 001 create new updateFromPE Dom!");
+
+    var originButtonContinue = document.getElementsByClassName("selected")[1];
+    var createDom = document.createElement('button');
+    var parentDom = originButtonContinue.parentNode;
+    createDom.id = "updateformPE";
+    createDom.innerText = "AA Miner Continue"
+    createDom.style.display = "inline-block";
+    originButtonContinue.style.display = "none";
+    parentDom.appendChild(createDom);
+
+    // send AA Miner API
+    // sendUpdateFromAAMiner();
+
+    // need to add background.js
+    // chrome.runtime.sendMessage('Hello Taiwan');
+
+};
+
+// AA Miner API
+function sendUpdateFromAAMiner() {
+    chrome.runtime.onMessage.addListener(function(response, sender, sendResponse){
+        alert(response);
+    });
+
+    // send API
+};
 
 // Do something after FB loading end
 setTimeout(function() {
-  console.log("!!! setTimeout 10000");
+  console.log("!!! 000 setTimeout 10000");
   changeReviewButton();
-}, 10000);
+}, 15000);
