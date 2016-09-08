b2c901871555deee080e99a128f0fc10c7b81b79
arvin_chen <arvin_chen@Arvin.local>
2016-09-08 18:34:48 +0800

    [refactor #1418]調整縮排大小

--- background.js
+++ background.js
@@ -1,52 +1,52 @@
 var options = {
-	domain : 'localhost'
+  domain : 'localhost'
 };
 
 chrome.cookies.getAll(options,function(data) {
-	var cookies = getCookieForRememberMe(data);
-	isRememberMe(cookies);
+  var cookies = getCookieForRememberMe(data);
+  isRememberMe(cookies);
 });
 
 function getCookieForRememberMe(data){
-	return data.filter(
-		function (info) {
-			return info.name == "remember_me"
-		});
+  return data.filter(
+    function (info) {
+      return info.name == "remember_me"
+    });
 }
 
 function isRememberMe(cookies){
-	if(cookies.length > 0){
-		onMessageForCheckRememberMe(true);
-	} else {
-		onMessageForCheckRememberMe(false);
-	}
+  if(cookies.length > 0){
+    onMessageForCheckRememberMe(true);
+  } else {
+    onMessageForCheckRememberMe(false);
+  }
 }
 
 function onMessageForCheckRememberMe(status){
-	chrome.runtime.onMessage.addListener(
-		function(request, sender, response) {
-			response(status);
-		}
-	);
+  chrome.runtime.onMessage.addListener(
+    function(request, sender, response) {
+      response(status);
+    }
+  );
 }
 
 chrome.tabs.onUpdated.addListener(checkUrl);
 
 function getDomain(url){
-	var host = "null";
-	if(typeof url == "undefined" || null == url)
-		url = window.location.href;
-	var regex = /.*\:\/\/([^\/]*).*/;
-	var match = url.match(regex);
-	if(typeof match != "undefined" && null != match)
-		host = match[1];
-	return host;
+  var host = "null";
+  if(typeof url == "undefined" || null == url)
+    url = window.location.href;
+  var regex = /.*\:\/\/([^\/]*).*/;
+  var match = url.match(regex);
+  if(typeof match != "undefined" && null != match)
+    host = match[1];
+  return host;
 }
 
 function checkUrl(tabId, changeInfo, tab) {
-	if(getDomain(tab.url).toLowerCase() == "business.facebook.com"){
-		chrome.browserAction.enable(tabId);
-	}else {
-		chrome.browserAction.disable(tabId);
-	}
+  if(getDomain(tab.url).toLowerCase() == "business.facebook.com"){
+    chrome.browserAction.enable(tabId);
+  }else {
+    chrome.browserAction.disable(tabId);
+  }
 };
--- popup.js
+++ popup.js
@@ -8,6 +8,7 @@ function getCurrentTabUrl(callback) {
   initialLoginButton();
   initialLogoutButton();
   callback();
+
 };
 
 function initialLoginButton(){
